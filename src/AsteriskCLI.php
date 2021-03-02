251c8ae3734cf683d6bd8a3ecf0e6aee221de281
vijaykumar <vijaykumar@drishti-soft.com>
2021-03-02 21:37:32 +0530

    sip show registry support added

--- src/AsteriskCLI.php
+++ src/AsteriskCLI.php
@@ -42,5 +42,81 @@ class AsteriskCLI
 
         return $data;
     }
+
+    function getDialPlanGlobals()
+    {
+        $rx = 'dialplan show globals';
+        $result = $this->execute($rx, false);
+        $list = explode("\n", $result);
+        array_pop($list);
+        array_pop($list);
+
+        $data = array();
+        foreach ($list as $line) {
+            $line = trim($line);
+            $arr = explode("=", $line);
+
+            if (isset($arr[0]) && isset($arr[1])) {
+                $data[$arr[0]] = $arr[1];
+            }
+        }
+        return $data;
+    }
+
+    function getSipRegistryStatus()
+    {
+        $rx = 'sip show registry';
+        $result = $this->execute($rx, false);
+        $list = explode("\n", $result);
+        $line = $list[0];
+
+        $headersRange = array();
+
+        $headersRange['Host'] = array(
+            strpos($line, "Host"),
+            strpos($line, "dnsmgr") - strpos($line, "Host")
+        );
+        $headersRange['dnsmgr'] = array(
+            strpos($line, "dnsmgr"),
+            strpos($line, "Username") - strpos($line, "dnsmgr")
+        );
+        $headersRange['Username'] = array(
+            strpos($line, "Username"),
+            strpos($line, "Refresh") - strpos($line, "Username")
+        );
+        $headersRange['Refresh'] = array(
+            strpos($line, "Refresh"),
+            strpos($line, "State") - strpos($line, "Refresh")
+        );
+        $headersRange['State'] = array(
+            strpos($line, "State"),
+            strpos($line, "Reg.Time") - strpos($line, "State")
+        );
+        $headersRange['Reg.Time'] = array(
+            strpos($line, "Reg.Time")
+        );
+
+        unset($list[0]);
+        array_pop($list);
+        $data = array();
+        foreach ($list as $line) {
+            $item = array();
+            foreach ($headersRange as $field => $range) {
+
+                if (isset($range[0])) {
+
+                    if (isset($range[1])) {
+                        $item[$field] = trim(substr($line, $range[0], $range[1]));
+                    } else {
+                        $item[$field] = trim(substr($line, $range[0]));
+                    }
+                }
+            }
+            if ($item) {
+                $data[] = $item;
+            }
+        }
+        return $data;
+    }
 }
 ?>
